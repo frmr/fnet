b670e5ebbd0aeac843e7b915a64cc61baa79e817
Farmer <farmergeddon@gmail.com>
2014-03-19 15:20:51 +0000

    Fixed compiler warnings.

---
+++ source/frmr/fnet/fnet.h
@@ -0,0 +1,7 @@
+#ifndef FRMR_FARMNET_H
+#define FRMR_FARMNET_H
+
+#include "frmr/fnet/server.h"
+#include "frmr/fnet/client.h"
+
+#endif
---
+++ source/frmr/fnet/server.h
@@ -0,0 +1,63 @@
+#ifndef FRMR_SERVER_H
+#define FRMR_SERVER_H
+
+#include <string>
+#include <vector>
+#include <utility>
+
+#include "enet/enet_enet.h"
+
+using std::string;
+using std::vector;
+using std::pair;
+
+namespace frmr
+{
+	namespace fnet
+	{
+		class frmrServer
+		{
+		private:
+			class ClientInfo
+			{
+			private:
+				ENetPeer*   peer;
+				double      timeOutTimer;
+
+			public:
+				ENetPeer*   GetPeerRef() const;
+				double      GetTimeOutTimer() const;
+				void        ResetTimeOutTimer();
+				void        UpdateTimeOutTimer( const double elapsedTime );
+
+			public:
+				ClientInfo( ENetPeer* const peer );
+				~ClientInfo();
+			};
+
+		private:
+			ENetHost*           server;
+			string              name;
+			vector<ClientInfo>  clients;
+			const double        timeOutLimit;
+
+		private:
+			int                 GetClientIndexFromID( const unsigned int id ) const;
+
+		public:
+			void                                    Broadcast( const string &message, const bool reliable ) const;
+			unsigned int                            Ping( const unsigned int client ) const;
+			void                                    Send( const unsigned int client, const string &message, const bool reliable ) const;
+			void                                    SetName( const string &newName );
+			bool                                    Start( const int port );
+			void                                    Stop();
+			vector< pair<unsigned int, string> >    Update( const double elapsedTime );
+
+		public:
+			frmrServer( const double timeOutLimit );
+			~frmrServer();
+		};
+	}
+}
+
+#endif
